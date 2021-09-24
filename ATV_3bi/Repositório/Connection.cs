2a5fa37c0d8431ef1058a54c83fd1f1dc97c5aea
brendaberzin <brenda.berzin@etec.sp.gov.br>
2021-09-23 23:37:39 -0300

    Classe Connection

---
+++ ATV_3bi/Repositório/Connection.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATV_3bi.Repositório
+{
+    public class Connection
+    {
+        MySqlConnection cn = new MySqlConnection("Server=localhost;DataBase=bd3bi;user=root;pwd=bsb#13072004");
+        public static string msg;
+        public MySqlConnection ConectarBD()
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch(Exception erro)
+            {
+                msg = "Ocorreu um erro ao conectar" + erro.Message;
+            }
+            return cn;
+        }
+
+        public MySqlConnection DesconectarBD()
+        {
+            try
+            {
+                cn.Close();
+            }
+            catch(Exception erro)
+            {
+                msg = "Ocorreu um erro ao desconectar" + erro.Message;
+            }
+            return cn;
+        }
+    }
+}
